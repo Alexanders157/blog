645767b0fa88c88b017d16f99ceea1e0c9c47e0a
Alexanders157 <Alexander.157@yandex.ru>
2024-03-19 00:22:38 +0300

    Commit 5

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -9,11 +9,8 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class PostFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Post::class;
+
     public function definition()
     {
         return [
