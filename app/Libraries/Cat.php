3dceed34e71d099ac8f80e72a6e67449e02b6ad7
Alexanders157 <Alexander.157@yandex.ru>
2024-03-19 19:58:46 +0300

    Commit Pets

---
+++ app/Libraries/Cat.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Libraries;
+
+class Cat extends Pets implements PetsInterface
+{
+    private const PAWS = 4;
+    public string $breed;
+    public string $name;
+    public int|null $speed = null;
+
+    public function __construct(string $name, string $breed, int|null $speed = null)
+    {
+        $this->name = $name;
+        $this->breed = $breed;
+        $this->speed = $speed;
+    }
+
+    public function run()
+    {
+        if ($this->speed) {
+            return "Кот по кличке: {$this->name} породы: {$this->breed}, {$this->append()}, а затем побежал бежит со скоростью {$this->speed} км/ч и у него ".self::PAWS." лапы";
+        }
+        return "Без спидов";
+    }
+
+
+    private function append()
+    {
+        return "Кот сначала гулял";
+        //return "Кот по кличке: {$this->name} породы: {$this->breed}, гуляет со скоростью {$this->speed} км/ч";
+    }
+
+}
